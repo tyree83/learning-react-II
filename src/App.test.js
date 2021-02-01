0c9ed450cbd345ce73b5467441b03db1b18a4048
Tyree Christensen <tyree83@gmail.com>
2021-02-01 13:47:41 -0500

    chapter 13

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import HelloWorld from './HelloWorld';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<HelloWorld />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./HelloWorld.css"
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="helloContainer">
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
